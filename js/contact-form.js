4c8cc0c94a0a4189fedee115b2a84d2841a40704
Назарій Феній <nfenii02@gmail.com>
2018-05-24 13:11:29 +0300

    Form, that sends information to TG added

---
+++ js/contact-form.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $("#applicationButton").bind("click", function () {
+        const xhttp = new XMLHttpRequest();
+        const botToken = "607809653:AAFDFWG1y0-hgb5f1QcXq9fh9qlyf_eqeG0";
+        const chatId = "-184963404";
+
+        let messageText = "";
+        let phoneNumber = $("#uPhoneInput").val();
+        let mailAdress = $("#uMailInput").val();
+        let projectDescription = $("#pDescInput").val();
+
+        if (phoneNumber != "" && mailAdress != "" && projectDescription != "") {
+            messageText += "Номер телефону: " + phoneNumber +
+                "%0AЕлектронна адреса: " + mailAdress +
+                "%0AОпис проекту: " + projectDescription;
+            xhttp.open(`POST`, `https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&parse_mode=html&text=${messageText}`, false);
+            xhttp.send();
+        }
+    });
+});
